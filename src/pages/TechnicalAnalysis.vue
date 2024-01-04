fca000c80c47948227f57882d19fa50251e41bb8
Stefan Nuernberger <stefan@elexir.eu>
2024-01-04 15:33:13 +0100

    added rudimentary header list view

--- src/pages/TechnicalAnalysis.vue
+++ src/pages/TechnicalAnalysis.vue
@@ -1,37 +1,50 @@
 <template>
   <q-page padding>
-    <div class="q-pa-md row items-start q-gutter-md">
-      <q-card class="my-card" v-if="!headersAvailable">
-        <q-card-section>
-          <q-skeleton type="text" />
-        </q-card-section>
-        <q-separator dark inset />
-        <q-card-section>
-          <q-skeleton type="rect" />
-        </q-card-section>
-      </q-card>
-      <q-card
-        class="my-card"
-        v-else
-        v-for="header in allHeaders"
-        v-bind:key="header.index"
-      >
-        <q-card-section>
-          <div class="text-h6">{{ header.name }}</div>
-        </q-card-section>
-        <q-separator dark inset />
-        <q-card-section>
-          <q-select
-            color="white"
-            bg-color="primary"
-            filled
-            v-model="model"
-            :options="options"
-            label="Label"
-          >
-          </q-select>
-        </q-card-section>
-      </q-card>
+    <div class="q-ma-md row no-wrap">
+      <div class="col-4">
+        <q-toolbar class="bg-primary text-white shadow-2">
+          <q-toolbar-title>Data Columns</q-toolbar-title>
+        </q-toolbar>
+        <q-scroll-area visible style="height: 600px" class="col-4">
+          <q-list bordered v-if="!headersAvailable">
+            <q-item v-for="n in 9" v-bind:key="n">
+              <q-item-section avatar top>
+                <q-skeleton type="QAvatar"></q-skeleton>
+              </q-item-section>
+              <q-item-section>
+                <q-skeleton type="text"></q-skeleton>
+              </q-item-section>
+            </q-item>
+          </q-list>
+          <q-list v-else bordered>
+            <q-item
+              clickable
+              v-ripple
+              v-for="header in allHeaders"
+              v-bind:key="header.index"
+            >
+              <q-item-section avatar top>
+                <q-avatar icon="folder" color="primary" text-color="white" />
+              </q-item-section>
+              <q-item-section>
+                <q-item-label>{{ header.name }}</q-item-label>
+                <q-item-label caption lines="1">Caption line</q-item-label>
+              </q-item-section>
+            </q-item>
+          </q-list>
+        </q-scroll-area>
+      </div>
+      <q-scroll-area visible style="height: 600px" class="col-8">
+        <q-select
+          color="white"
+          bg-color="primary"
+          filled
+          v-model="model"
+          :options="options"
+          label="Label"
+        >
+        </q-select>
+      </q-scroll-area>
     </div>
   </q-page>
 </template>
